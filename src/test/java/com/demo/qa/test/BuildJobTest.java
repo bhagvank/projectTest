9bf9a4a6336bf77e386fe5179aac09c86058e536
Bhagvan Kommadi <bhagvank@gmail.com>
2021-07-06 01:37:45 +0530

    added new test method

--- src/test/java/com/demo/qa/test/BuildJobTest.java
+++ src/test/java/com/demo/qa/test/BuildJobTest.java
@@ -24,6 +24,43 @@ public class BuildJobTest {
 	}
 	
 	@Test
+	public void login()
+	{
+		driver.get("https://www.lambdatest.com/");
+		
+		
+		WebElement logInLink = driver.findElement(By.xpath("//a[contains(text(),'Log in')]"));
+		logInLink.click();
+		
+		
+		WebElement emailField = driver.findElement(By.xpath("//input[@placeholder='Email']"));
+		emailField.sendKeys("username@gmail.com");
+		
+		
+		WebElement passwordField = driver.findElement(By.xpath("//input[@placeholder='Password']"));
+		passwordField.sendKeys("Test@123");
+		
+		
+		WebElement loginButton = driver.findElement(By.xpath("//button[@class='btn btn-primary btn-lg btn-block mt-3']"));
+		loginButton.click();
+		
+		
+		String expectedURL = "https://accounts.lambdatest.com/dashboard";
+		String expectedPageTitle = "Welcome - LambdaTest";
+		
+		String actualURL = driver.getCurrentUrl();
+		String actualPageTitle = driver.getTitle();
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+					
+		//Assertion and verification of expected URL and actual URL
+		Assert.assertEquals(actualURL, expectedURL);
+		
+		//Assertion and verification of expected Page Title and actual Page Title
+		Assert.assertEquals(actualPageTitle, expectedPageTitle);
+	}
+	
+	/*@Test
 	public void ToolsQaTest()
 	{
 		driver.findElement(By.xpath("(//div[@class=\"card-up\"])[1]")).click();
@@ -31,7 +68,7 @@ public class BuildJobTest {
 		String actualtext=driver.findElement(By.xpath("(//div[contains(text(),\"Elements\")])[1]")).getText();
 		Assert.assertEquals(expectedtext, actualtext, "Message is not Displaying as expected");
 	}
-	
+	*/
 	@AfterMethod
 	public void tearDown()
 	{
